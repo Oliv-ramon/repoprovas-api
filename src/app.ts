33b717d1b5361d9611e81e4a1246b56c1cc69b1a
ramon oliveira <ramon30012000@gmail.com>
2022-04-24 15:39:08 -0300

    feat: add schema validation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import router from "./routes/index.js";
+import "express-async-error";
+import { errorHandlerMiddleware } from "./middlewares/index.js";
 dotenv.config()
 
 const app = express();
 app.use(json());
 app.use(cors());
-app.get("/", (req, res) => res.sendStatus(200));
+app.use(router);
+app.use(errorHandlerMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`running on ${process.env.PORT}`);
